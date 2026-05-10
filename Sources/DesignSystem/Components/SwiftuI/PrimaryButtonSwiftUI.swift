b76d0065bdc2caa1ddaf3ce4fa47d63e71d46143
Ajay Odedara <ajodedra16@gmail.com>
2026-05-10 10:02:07 +0100

    Added swiftui button as public

--- Sources/DesignSystem/Components/SwiftuI/PrimaryButtonSwiftUI.swift
+++ Sources/DesignSystem/Components/SwiftuI/PrimaryButtonSwiftUI.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct PrimaryButtonSwiftUI: View {
+public struct PrimaryButtonSwiftUI: View {
     let title: String
     let action: () -> Void
  
-    var body: some View {
+    public var body: some View {
         Button(title, action: action)
             .buttonStyle(.bordered)
     }
